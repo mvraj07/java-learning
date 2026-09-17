9d10ae0c57e3c8bf1e2bc05e95b692256150ba96
Raj Mahajan <mvraj7898@gmail.com>
2026-09-17 23:18:43 +0530

    my first few projects created

---
+++ hello.java
@@ -0,0 +1,30 @@
+// import java.util.Scanner;
+public class hello{
+    public static void main(String[] raj){
+
+System.out.println();
+
+        // Scanner sc = new Scanner(System.in);
+        // String name = "raj";
+        // int age;
+        // int integer;
+        // float floating;
+        // boolean truth;
+        // String name;
+        // name = sc.next();
+        // truth = sc.hasNextInt();
+        // age = sc.nextInt();
+        // System.out.print(name);
+        // System.out.print(truth);
+
+        // double cgpa = 7.22;
+        // boolean automation = true;
+        // System.out.println("Name: "+name);
+        // System.out.println("CGPA: "+cgpa);
+        // System.out.println("Learning: "+automation);
+        // System.out.println("Age: "+age);
+        // System.out.println(name);
+    //    sc.close();
+
+    }
+}
---
+++ variablesDemo.java
@@ -0,0 +1,26 @@
+public class variablesDemo{
+    public static void main(String[] args){
+        String name="Raj";
+        char grade ='A';
+        float cgpa=7.4f;
+        double percentage =92.60;
+        int rollno = 24; 
+        boolean automation = true; 
+
+        System.out.println("Name: "+name);
+        System.out.println("grade: "+grade);
+        System.out.println("cgpa: "+cgpa);
+        System.out.println("Percentage: "+percentage);
+        System.out.println("automation: "+automation);
+        System.out.println("A\"\\\"");
+        int a = 3;
+double b = 3.5;
+System.out.println(a+b+","+(a-b)+","+(a*b)+","+(a/b)+","+(a%b));
+int x = 10;
+float y = 3;
+
+System.out.println(x / y);
+System.out.println(x % y);
+
+    }
+}
