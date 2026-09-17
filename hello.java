09a31563d6aec71adf9fc32e26d9633a7eb4426f
Raj Mahajan <mvraj7898@gmail.com>
2026-09-17 23:22:35 +0530

    fnkfge

--- hello.java
+++ hello.java
@@ -3,7 +3,7 @@ public class hello{
     public static void main(String[] raj){
 
 System.out.println();
-
+raj
         // Scanner sc = new Scanner(System.in);
         // String name = "raj";
         // int age;
